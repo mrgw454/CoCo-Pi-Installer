25f7f3f6c64831c4d6e20eeaf20896644dddef44
mrgw454 <ron@kdomain.org>
2024-12-12 13:21:10 -0600

    update menu script

--- fix-cocopi.sh
+++ fix-cocopi.sh
@@ -124,6 +124,24 @@ else
 fi
 
 
+# check for fix
+fix="fix-20241212-03"
+if grep -q "$fix" $file; then
+    echo fix $fix already complete.
+    echo
+else
+    echo Applying fix $fix...
+    echo
+
+	cp $HOME/CoCo-Pi-Installer/update/20241212/.mame/CoCoPi-menu-Utilities-Administration.sh $HOME/.mame
+
+    cd $HOME
+
+    echo "$fix" >>$file
+    echo
+fi
+
+
 echo
 echo
 echo Please reboot as soon as possible so all updates can be applied.  Thank you.
