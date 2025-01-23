f42fbf9aafb39f1e88ec3a7b99c8dce8514c7ae6
mrgw454 <ron@kdomain.org>
2025-01-23 12:07:48 -0600

    update scripts

--- update/20250123/source/make-toolshed-code.sh
+++ update/20250123/source/make-toolshed-code.sh
@@ -4,23 +4,22 @@ cd $HOME/source
 
 # if a previous toolshed folder exists, move into a date-time named folder
 
-if [ -d "toolshed" ]; then
+if [ -d "toolshed-code" ]; then
 
         foldername=$(date +%Y-%m-%d_%H.%M.%S)
 
-        mv "toolshed" "toolshed-$foldername"
+        mv "toolshed-code" "toolshed-code-$foldername"
 
-        echo -e Archiving existing toolshed folder ["toolshed"] into backup folder ["toolshed-$foldername"]
+        echo -e Archiving existing toolshed-code folder ["toolshed-code"] into backup folder ["toolshed-code-$foldername"]
         echo -e
         echo -e
 fi
 
-# https://github.com/nitros9project/toolshed
-git clone https://github.com/nitros9project/toolshed.git
+# https://sourceforge.net/p/toolshed/code/ci/default/tree/
+hg clone http://hg.code.sf.net/p/toolshed/code toolshed-code
+cd $HOME/source/toolshed-code
 
-cd $HOME/source/toolshed
-
-GITREV=`git rev-parse --short HEAD`
+hg pull
 
 echo $(nproc) / 2 | bc
 cores=$(echo $(nproc) / 2 | bc)
@@ -33,12 +32,12 @@ make -j$cores -C dwdos
 make -j$cores -C hdbdos
 make -j$cores -C superdos
 
-cd $HOME/source/toolshed/build/unix
+cd $HOME/source/toolshed-code/build/unix
 
 sudo chmod a-x unittest/*
 sudo find . -executable -type f -exec cp {} /usr/local/bin \;
 
-cd $HOME/source/toolshed
+cd $HOME/source/toolshed-code
 sudo cp d2o /usr/local/bin
 sudo cp d2u /usr/local/bin
 sudo cp o2u /usr/local/bin
@@ -48,3 +47,4 @@ sudo cp u2o /usr/local/bin
 echo
 echo
 echo Done!
+
