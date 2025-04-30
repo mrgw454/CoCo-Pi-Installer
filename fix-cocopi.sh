3cb88a54446dd33006a7506349fe7bfe1e37c52c
mrgw454 <ron@kdomain.org>
2025-04-30 12:35:41 -0500

    latest MAME package and other minor updates

--- fix-cocopi.sh
+++ fix-cocopi.sh
@@ -623,6 +623,26 @@ else
 fi
 
 
+# check for fix
+fix="fix-20250430-01"
+if grep -q "$fix" $file; then
+    echo fix $fix already complete.
+    echo
+else
+    echo Applying fix $fix...
+    echo
+
+    cp $HOME/CoCo-Pi-Installer/update/20250430/scripts/*.sh $HOME/scripts
+    cp $HOME/CoCo-Pi-Installer/update/20250430/source/*.sh $HOME/source
+    cp $HOME/CoCo-Pi-Installer/update/20250430/Pictures/*.* $HOME/Pictures
+
+    cd $HOME
+
+    echo "$fix" >>$file
+    echo
+fi
+
+
 echo
 echo
 echo Please reboot as soon as possible so all updates can be applied.  Thank you.
